6293d5f3c858ece3054186c61acf2f70358a3fe4
Terence Jeong <terence.jeong@gmail.com>
2018-01-24 21:18:06 +1100

    cool stuff

--- api/server/db/mongoose.js
+++ api/server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 const keys = require('../config/keys')
 
 mongoose.Promise = global.Promise;
-mongoose.connect( keys.MONGO_URI,  {
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/Notes', {
   useMongoClient: true})
 
 module.exports = mongoose
